ea24779d146f709c9c58e6394905f03409de254e
paula <paularosarodriguezmorales03@gmail.com>
2023-10-14 22:18:56 +0100

    First commit of Kata 1. Creation of class Person.

---
+++ src/software/ulpgc/kata1/Person.java
@@ -0,0 +1,37 @@
+package software.ulpgc.kata1;
+
+import java.time.LocalDate;
+
+public class Person {
+    private final String name;
+    private final LocalDate birthday;
+
+    public Person(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public int getAge() {
+        return toYears(LocalDate.now().toEpochDay() / birthday.toEpochDay());
+    }
+
+    private int toYears(long days) {
+        return (int) (days/365.25);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+}
